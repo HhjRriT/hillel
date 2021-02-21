a6d83484b3b50804b9c87ce09933933bea2145f7
HhjRriT <artkol0709@gmail.com>
2021-02-22 01:24:50 +0200

    test

--- test git.js
+++ test git.js
@@ -1,1 +1,16 @@
-// some
+const num1 = +prompt("введите первое число");
+const num2 = +prompt("введите второе число");
+if (num1 > num2) {
+    alert(num1 + " больше чем " + num2);
+} else if (num1 < num2) {
+    alert(num2 + " больше чем " + num1);
+} else {
+    alert("числа равны");
+}
+
+const numN = +prompt("дайте число") % 10;
+if (numN % 2 === 1) {
+    alert("число заканьчивается на " + numN + " не четное");
+} else {
+    alert("число заканьчивается на " + numN + " четное");
+}
