8a8ddebc1998e7b170e7ffe6e82f5712fb2821ba
HhjRriT <artkol0709@gmail.com>
2021-03-11 00:23:22 +0200

    added hw10

--- hw9/hw9.js
+++ hw9/hw9.js
@@ -7,14 +7,6 @@ const addMe = document.createElement("button");
 
 addMe.type = "button";
 addMe.innerText = "addMe";
-addMe.addEventListener("click", function () {
-    users.push(input.value);
-    console.log(users);
-    let newOne = document.createElement("li");
-    newOne.innerText = input.value;
-    list.appendChild(newOne);
-    input.value = null;
-})
 mainDiv.appendChild((form));
 form.appendChild(input);
 form.appendChild(addMe);
@@ -29,7 +21,10 @@ function makeAList(arr) {
     }
     return ul;
 }
+const list = makeAList(users);
+mainDiv.appendChild(list);
 
-
-const list = makeAList(users)
-mainDiv.appendChild(list)
+addMe.addEventListener("click", function () {
+    users.push(input.value);
+    mainDiv.replaceChild(makeAList(users), mainDiv.lastChild);
+})
